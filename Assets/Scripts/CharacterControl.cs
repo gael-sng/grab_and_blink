34c8fc01aa8f48ebf1fec66f8aee842ba305f636
CristianoChiaramelli <cristianochiaramelli@gmail.com>
2016-10-29 16:26:44 -0200

    Adicionei as paradas

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+
+    private GameObject head;
+    private Rigidbody rb;
+
+    public float walkSpeed, rotateSpeed;
+
+    // Use this for initialization
+    void Start () {
+        head = transform.GetChild(0).gameObject;
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 direction = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+        rb.velocity = (walkSpeed * direction);
+        
+        transform.Rotate(new Vector3(0, rotateSpeed * Input.GetAxis("Mouse X"), 0));
+        head.transform.Rotate(new Vector3(-rotateSpeed * Input.GetAxis("Mouse Y"), 0, 0));
+	}
+}

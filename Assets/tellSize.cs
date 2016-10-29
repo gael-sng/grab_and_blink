9ae091182efff18d0120b584d64c8a9a8f29e4d2
elisasaltori <elisa.trujillo@usp.br>
2016-10-29 19:00:05 -0200

    Lvl1BaseOB

---
+++ Assets/tellSize.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tellSize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(GetComponent<Collider>().bounds.size);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
